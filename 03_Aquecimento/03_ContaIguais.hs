662ee4c4eccc89ab9b554b08c1f8888a12b3be55
Joaonorr <joaonoroes@alu.ufc.br>
2022-07-14 20:14:13 -0300

    fix: contaIguais

--- 03_Aquecimento/03_ContaIguais.hs
+++ 03_Aquecimento/03_ContaIguais.hs
@@ -7,3 +7,16 @@
 -- iguais 2 3 2 == 2
 -- iguais 2 1 1 == 2
 -- iguais 3 2 1 == 0
+
+iguais :: Int -> Int -> Int -> Int
+iguais a b c
+  | a == b && b == c = 3
+  | a == b || b == c || a == c = 2
+  | otherwise = 0
+
+
+main = do
+    a <- readLn :: IO Int
+    b <- readLn :: IO Int
+    c <- readLn :: IO Int
+    print $ iguais a b c
