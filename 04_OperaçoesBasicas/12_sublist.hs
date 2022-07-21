4c8d4e2eb97ff6694189cd93c0c224f24238a73c
Joaonorr <joaonoroes@alu.ufc.br>
2022-07-21 12:43:34 -0300

    feat: sublist

--- 04_OperaçoesBasicas/12_sublist.hs
+++ 04_OperaçoesBasicas/12_sublist.hs
@@ -10,20 +10,13 @@
 -- sublist -4 -2 [0,1,2,3,4,5] == [2,3]
 sublist :: Int -> Int -> [Int] -> [Int]
 sublist a b xs
-            | a < 0 = drop (length xs - a) (take b xs)
-            | b < 0 = drop a (take (length xs - b) xs)
-            | a < 0 && b < 0 = drop (length xs - a) (take (length xs - b) xs)
+            | a < 0 && b < 0 = drop (length xs + a) (take (length xs + b) xs)
+            | a < 0 = drop (length xs + a) (take b xs)
+            | b < 0 = drop a (take (length xs + b ) xs)
             | otherwise = drop a (take b xs)
 
 main = do
-    print $ sublist 1 3 [0,1,2,3,4,5,6,7,8,9,10] -- [1,2]
-    print $ sublist 0 11 [0,1,2,3,4,5,6,7,8,9,10] -- [0,1,2,3,4,5,6,7,8,9,10]
-    print $ sublist 2 8 [0,1,2,3,4,5,6,7,8,9,10] -- [2,3,4,5,6,7]
-    print $ sublist 0 (-1) [0,1,2,3,4,5,6,7,8,9,10] -- [0,1,2,3,4,5,6,7,8,9]
-    print $ sublist 2 (-2) [0,1,2,3,4,5,6,7,8,9,10] -- [2,3,4,5,6,7,8]
-    print $ sublist (-10) (-1) [0,1,2,3,4,5,6,7,8,9,10] -- [1,2,3,4,5,6,7,8,9]
-    print $ sublist (-4) (-2) [0,1,2,3,4,5] -- [2,3]
---     a <- readLn :: IO Int
---     b <- readLn :: IO Int
---     c <- readLn :: IO [Int]
---     print $ sublist a b c
+    a <- readLn :: IO Int
+    b <- readLn :: IO Int
+    c <- readLn :: IO [Int]
+    print $ sublist a b c
